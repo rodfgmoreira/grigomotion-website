7ca2bf1911036e06884b792ab7b711e5310ddfdc
rodfgmoreira <rodfgmoreira@gmail.com>
2025-03-06 12:20:49 -0300

    Atualização Página Sobre

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -5,7 +5,7 @@ export default function Contatos() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-black mt-[-6rem] px-6 py-10">
       <h1 className="text-3xl font-bold text-center">
-        Henrique Grigoletto - Editor de Vídeo e Motion Designer
+        Henrique Grigoletto - Motion Designer
       </h1>
       <p className="text-2xl font-normal text-center mb-6">grigomotion@gmail.com</p>
 
